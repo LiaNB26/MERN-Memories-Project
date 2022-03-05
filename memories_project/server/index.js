6cc615273cdfa2b21015ca79f2b8cba31d8f6a5d
Lia <liabrachio@gmail.com>
2022-03-05 13:06:07 +0200

    server: post schema, post routes and logic

--- memories_project/server/index.js
+++ memories_project/server/index.js
@@ -4,16 +4,20 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+import postRoutes from './routes/posts.js';
+
 dotenv.config();
-const app = express();
 
 const PORT = process.env.PORT || 5000;
+const CONNECTION_URL = process.env.DATABASE_CONNECTION_URL;
+
+const app = express();
+
+app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
-
-const CONNECTION_URL = process.env.DATABASE_CONNECTION_URL;
 
 mongoose
   .connect(CONNECTION_URL, {
